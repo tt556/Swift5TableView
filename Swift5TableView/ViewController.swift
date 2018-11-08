2f4db1aa16a3450ab35f6c38f9605f8fca783545
tt556 <so888tk@gmail.com>
2018-11-08 23:13:12 +0900

    initialize commit

--- Swift5TableView/ViewController.swift
+++ Swift5TableView/ViewController.swift
@@ -10,11 +10,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var label: UILabel!
+    
+    var box = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func countUp(_ sender: Any) {
+        
+        box = box + 1
+        label.text = String(box)
+        
+        if box > 10{
+            
+            //画面遷移させるコードを記述する
+            let viewController2:ViewController2 = self.storyboard?.instantiateViewController(withIdentifier: "next") as! ViewController2
+            
+            viewController2.count = box
+            self.navigationController?.pushViewController(viewController2, animated: true)
+            
+            
+        }
+        
+    }
+    
 }
 
---
+++ Swift5TableView/ViewController3.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController3.swift
+//  Swift5TableView
+//
+//  Created by 神崎泰旗 on 2018/11/08.
+//  Copyright © 2018年 taiki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController3: UIViewController {
+
+    
+    var count2 = 0
+    
+    @IBOutlet var label: UILabel!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        label.text = String(count2)
+
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
